991ca4777453104c94d49172f9ccf26f0de9c8a6
harshit <harshit2k19@gmail.com>
2025-12-07 10:21:49 +0530

    Basic compilation pipeline.

---
+++ compilation/main.cpp
@@ -0,0 +1,7 @@
+#include "source.hpp"
+#include <iostream>
+int main()
+{
+    std::cout<<add(2, 3)<<std::endl;
+    return 0;
+}
---
+++ compilation/source.cpp
@@ -0,0 +1,5 @@
+#include "source.hpp"
+int add(int a,int b)
+{
+    return a+b;
+}
